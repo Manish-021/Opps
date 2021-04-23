402ee8fbf8d61f940ce254e90081484f9a1a35ad
mk261 <manish.kumar_cs19@gla.ac.in>
2021-04-23 21:02:11 +0530

    create Main.java

---
+++ src/VideoRentalInventorSystem/execution/Main.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Manish Kumar
+ *   Date: 23-04-2021
+ *   Time: 20:59
+ *   File: Main.java
+ */
+
+package VideoRentalInventorSystem.execution;
+
+public class Main {
+}
