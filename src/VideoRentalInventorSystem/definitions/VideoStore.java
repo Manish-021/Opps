e7986c309429280b0dd440deaedd05930cd2f9a9
mk261 <manish.kumar_cs19@gla.ac.in>
2021-05-02 15:59:05 +0530

    add components of the defination of the VideoStore.java

--- src/VideoRentalInventorSystem/definitions/VideoStore.java
+++ src/VideoRentalInventorSystem/definitions/VideoStore.java
@@ -7,6 +7,41 @@
 
 package VideoRentalInventorSystem.definitions;
 
+import java.util.Arrays;
+
 public class VideoStore {
     private Video[] store;
+
+    public VideoStore() {
+        this.store = new Video[5];
+    }
+
+    public Video[] getStore() {
+        return store.clone();//.clone returns the copy of an array
+        //not the array  reference itself.
+    }
+
+    public void setStore(Video[] store) {
+        this.store = store;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoStore{" +
+                "store=" + Arrays.toString(store) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VideoStore that = (VideoStore) o;
+        return Arrays.equals(getStore(), that.getStore());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getStore());
+    }
 }
